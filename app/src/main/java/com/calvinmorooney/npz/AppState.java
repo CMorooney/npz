0baaff633d57cfdfd99bd7b521f63cbd0e2fe5cb
Calvin Morooney <calvinmorooney@openarc.net>
2015-04-11 23:38:47 -0400

    add app version number to installation object, I feel like that might come in handy

--- app/src/main/java/com/calvinmorooney/npz/AppState.java
+++ app/src/main/java/com/calvinmorooney/npz/AppState.java
@@ -22,6 +22,7 @@ public class AppState extends Application {
 
     final String CLIENT_ID = "LyfrTXo1XlbfMhgbxbCSsDrRavB4QxjIJlDCjerH";
     final String APP_ID = "vaAZ53fv0UAg48EPvPHvSoBBiBQFjV9y6HqQvsqF";
+    final int VERSION = 1;
 
     public ArrayList<String> deviceDirectory;
     public List<ParseUser> friendsList;
@@ -49,16 +50,21 @@ public class AppState extends Application {
             public void done(ParseException e) {
                 if (e == null) {
                     Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
-
-                    // Associate the device with a user
-                    ParseInstallation installation = ParseInstallation.getCurrentInstallation();
-                    installation.put("username", ParseUser.getCurrentUser().getUsername());
-                    installation.saveInBackground();
                 } else {
                     Log.e("com.parse.push", "failed to subscribe for push", e);
                 }
             }
         });
+    }
+
+    private void updateInstallation()
+    {
+        // Associate the device with a user
+        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+        installation.put("username", ParseUser.getCurrentUser().getUsername());
+        installation.put("user", ParseUser.getCurrentUser());
+        installation.put("appVersion", VERSION);
+        installation.saveInBackground();
     }
 
     private void loadPhoneList ()
